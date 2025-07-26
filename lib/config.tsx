9204413d49784f395e619beaaab50ee655ad449c
Paradox281 <kharismatio220@gmail.com>
2025-07-26 15:31:53 +0700

    .

--- lib/config.tsx
+++ lib/config.tsx
@@ -1,4 +1,4 @@
-export const API_BASE_URL = process.env.NEXT_PUBLIC_API_BASE_URL || 'http://localhost:8080';
+export const API_BASE_URL = process.env.NEXT_PUBLIC_API_BASE_URL;
 
 export const API_ENDPOINTS = {
   TOUR_PACKAGE_DISKON: '/api/tour-package-diskon',
